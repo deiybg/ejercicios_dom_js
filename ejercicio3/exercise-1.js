9651094f0de0f2e3291790ac03edeeccfacb917d
deiybg <deibygorrin@gmail.com>
2024-05-25 00:28:12 +0200

    ejercicio 3 terminado por completo

--- ejercicio3/exercise-1.js
+++ ejercicio3/exercise-1.js
@@ -8,7 +8,6 @@ for (const country of countries) {
   const li = document.createElement("li");
   li.textContent = country;
   ul.appendChild(li);
-
 }
 document.body.appendChild(ul);
 /////////////////////////////////ejercicio 3.2///////////////////////////////////
@@ -36,40 +35,45 @@ dataFunction.appendChild(ulCars);
 /////////////////////////////////ejercicio 3.4///////////////////////////////////
 
 const cities = [
-	{title: 'Random title', imgUrl: 'https://picsum.photos/300/200?random=1'}, 
-	{title: 'Random title', imgUrl: 'https://picsum.photos/300/200?random=2'},
-	{title: 'Random title', imgUrl: 'https://picsum.photos/300/200?random=3'},
-	{title: 'Random title', imgUrl: 'https://picsum.photos/300/200?random=4'},
-	{title: 'Random title', imgUrl: 'https://picsum.photos/300/200?random=5'}
+  { title: "Random title", imgUrl: "https://picsum.photos/300/200?random=1" },
+  { title: "Random title", imgUrl: "https://picsum.photos/300/200?random=2" },
+  { title: "Random title", imgUrl: "https://picsum.photos/300/200?random=3" },
+  { title: "Random title", imgUrl: "https://picsum.photos/300/200?random=4" },
+  { title: "Random title", imgUrl: "https://picsum.photos/300/200?random=5" },
 ];
 
 for (const city of cities) {
+  const divCities = document.createElement("div");
+  const h4 = document.createElement("h4");
+  h4.textContent = city.title;
+  divCities.appendChild(h4);
+  const image = document.createElement("img");
+  image.src = city.imgUrl;
+  image.alt = city.title;
+  divCities.appendChild(image);
+
+  const buttonClear = document.createElement("button");
+  buttonClear.classList.add("buttonclear");
+  buttonClear.textContent = "Eliminar este div";
+  divCities.appendChild(buttonClear);
+
+  buttonClear.addEventListener("click", () => {
+    divCities.remove();
     
-    const divCities = document.createElement("div");
-    const h4 = document.createElement("h4");
-    h4.textContent = city.title;
-    divCities.appendChild(h4)
-    const image = document.createElement("img");
-    image.src = city.imgUrl;
-    image.alt = city.title;
-    divCities.appendChild(image);
-    document.body.appendChild(divCities);
-}   
+  });
+  document.body.appendChild(divCities);
+}
 
 /////////////////////////////////ejercicio 3.5///////////////////////////////////
 
 const button = document.createElement("button");
-        button.textContent = "Eliminar último div";
-        button.classList.add("btn");
-        document.body.appendChild(button);
+button.textContent = "Eliminar último div";
+button.classList.add("btn");
+document.body.appendChild(button);
 
-        button.addEventListener("click", () => {
-            const divs = document.querySelectorAll("body > div");
-            if (divs) {
-                divs[divs.length - 1].remove();
-            }
-        });
-
-
-
-
+button.addEventListener("click", () => {
+  const divs = document.querySelectorAll("body > div");
+  if (divs) {
+    divs[divs.length - 1].remove();
+  }
+});
